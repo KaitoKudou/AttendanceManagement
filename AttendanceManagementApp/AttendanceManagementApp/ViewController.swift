0585b3904165f5b0ab0a8cb6884117657f37ef8b
KaitoKudou <kaitokudou2468110921@gmail.com>
2020-07-29 02:30:48 +0900

    CarthageでAPIKitを導入

---
+++ AttendanceManagementApp/AttendanceManagementApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AttendanceManagementApp
+//
+//  Created by 工藤海斗 on 2020/07/29.
+//  Copyright © 2020 工藤海斗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
