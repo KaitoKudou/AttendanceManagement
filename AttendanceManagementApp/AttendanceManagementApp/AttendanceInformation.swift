14cb0bef9879004dd1e2a9e69c6d988167860889
KaitoKudou <kaitokudou2468110921@gmail.com>
2020-07-30 03:16:00 +0900

    http通信を許可した

---
+++ AttendanceManagementApp/AttendanceManagementApp/AttendanceInformation.swift
@@ -0,0 +1,9 @@
+//
+//  AttendanceInformation.swift
+//  AttendanceManagementApp
+//
+//  Created by 工藤海斗 on 2020/07/30.
+//  Copyright © 2020 工藤海斗. All rights reserved.
+//
+
+import Foundation
---
+++ AttendanceManagementApp/AttendanceManagementApp/FetchAttendanceInformationRequest.swift
@@ -0,0 +1,9 @@
+//
+//  FetchAttendanceInformationRequest.swift
+//  AttendanceManagementApp
+//
+//  Created by 工藤海斗 on 2020/07/30.
+//  Copyright © 2020 工藤海斗. All rights reserved.
+//
+
+import Foundation
